64901e61fee64bc4dc7a3da7cc51c15e194ef186
Evan Burrell <burrelle@oregonstate.edu>
2019-01-04 09:19:07 +0000

    ESLint configuration change

--- 4. Array Cardio Day 1/index.js
+++ 4. Array Cardio Day 1/index.js
@@ -1,7 +1,6 @@
 /* eslint no-param-reassign:
   ["error", { "props": true, "ignorePropertyModificationsFor": ["obj"] }]
 */
-/* eslint implicit-arrow-linebreak: [, "beside"] */
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
